1e7a3295446a822d54b61889ee619de55e3c9c1a
bea <beatriztattooer@gmail.com>
2024-01-24 20:57:21 -0300

    :construction: quasoando 🥐

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,37 +1,56 @@
 <template>
-  <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
-  </div>
+	<div>
+		<q-item-section>
+			<q-item-label>
+				{{ title }}
+			</q-item-label>
+			<q-item-label caption>
+				<q-badge
+					color="blue-4"
+					text-color="white"
+				>
+					nao sei o que to fazendo /ᐠ - ˕ -マ
+					<q-icon
+						name="thumb_up"
+						size="14px"
+						class="q-ml-xs"
+					/>
+				</q-badge>
+			</q-item-label>
+		</q-item-section>
+
+		<ul>
+			<li
+				v-for="todo in todos"
+				:key="todo.id"
+			>
+				<input
+					type="checkbox"
+					name="todo"
+				/>
+				{{ todo.content }}
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { Todo, Meta } from './models';
+import { computed } from 'vue';
+import { Todo } from './models';
 
 interface Props {
-  title: string;
-  todos?: Todo[];
-  meta: Meta;
-  active: boolean;
+	title: string;
+	todos?: Todo[];
 }
 const props = withDefaults(defineProps<Props>(), {
-  todos: () => [],
+	todos: () => [],
 });
 
-const clickCount = ref(0);
-function increment() {
-  clickCount.value += 1;
-  return clickCount.value;
-}
-
 const todoCount = computed(() => props.todos.length);
-
 </script>
+
+<style scoped>
+ul > li {
+	list-style-type: none;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,43 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+	<q-page class="row items-center justify-evenly">
+		<carousel-index />
+		<example-component
+			title="🥐 TODO"
+			:todos="todos"
+			:meta="meta"
+		/>
+	</q-page>
 </template>
 
 <script setup lang="ts">
 import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/ExampleComponent.vue';
+import CarouselIndex from 'src/components/CarouselIndex.vue';
 import { ref } from 'vue';
 
 const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
+	{
+		id: 1,
+		content: 'Ir comprar pão',
+	},
+	{
+		id: 2,
+		content: 'Lavar roupa',
+	},
+	{
+		id: 3,
+		content: 'Dormir',
+	},
+	{
+		id: 4,
+		content: 'Assistir HunterXHunter',
+	},
+	{
+		id: 5,
+		content: 'Procurar trabaio',
+	},
 ]);
 const meta = ref<Meta>({
-  totalCount: 1200
+	totalCount: 1200,
 });
 </script>
